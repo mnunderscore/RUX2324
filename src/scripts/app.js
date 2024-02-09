449ede394602fad641a788db2b0e1832bdc77e7d
mnunderscore <33391370+mnunderscore@users.noreply.github.com>
2024-02-09 14:38:49 +0100

    Add jquery dependencies, update font size in _type.scss, import jquery in app.js, and update styles in app.scss

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,17 +1,18 @@
 "use strict";
-"use strict";
+// Import jQuery
+import $ from 'jquery';
 
 // Function to animate .paper:hover::before
 function animatePaper() {
     // Get the .paper element
-    const paperElement = document.querySelector('.corner');
+    const cornerElement = document.querySelector('.corner');
 
     // Add a class to trigger the animation
-    paperElement.classList.toggle('animate-paper');
+    cornerElement.classList.toggle('animate-paper');
 }
 
 // Attach the onclick event to trigger the animation
 document.addEventListener('DOMContentLoaded', () => {
-    const paperElement = document.querySelector('.corner');
-    paperElement.addEventListener('click', animatePaper);
+    const cornerElement = document.querySelector('.corner');
+    cornerElement.addEventListener('click', animatePaper);
 });
