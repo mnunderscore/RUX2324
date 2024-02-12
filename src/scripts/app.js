44724001ca12f3d6d24fece0351211b05a040632
mnunderscore <33391370+mnunderscore@users.noreply.github.com>
2024-02-12 10:07:27 +0100

    Refactor post-it component and update styles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,23 +2,19 @@
 // Import jQuery
 import $ from 'jquery';
 
-// Function to animate .paper:hover::before
-function animatePaper() {
-    // Get the .paper element
-    const cornerElement = document.querySelector('.corner');
-
-    // Add a class to trigger the animation
-    cornerElement.classList.toggle('animate-paper');
+function animateCorner() {
+    const cornerElement = document.querySelector('.post-it__corner');
+    cornerElement.classList.toggle('--animatecorner');
 }
 
 function toggleVisibility() {
-    const navElement = document.querySelector('.nav');
-    navElement.classList.toggle('toggleVisibility');
+    const navElement = document.querySelector('.post-it__nav');
+    navElement.classList.toggle('--toggleVisibility');
 }
 
 // Attach the onclick event to trigger the animation
 document.addEventListener('DOMContentLoaded', () => {
-    const cornerElement = document.querySelector('.corner');
-    cornerElement.addEventListener('click', animatePaper);
+    const cornerElement = document.querySelector('.post-it__corner');
+    cornerElement.addEventListener('click', animateCorner);
     cornerElement.addEventListener('click', toggleVisibility);
 });
