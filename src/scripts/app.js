20bc81b9fe11c645953b225126dec518d0e83761
mnunderscore <33391370+mnunderscore@users.noreply.github.com>
2024-02-14 14:37:21 +0100

    Update footer slider IDs and add functionality to setActivePage

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,33 +2,72 @@
 // Import jQuery
 import $ from 'jquery';
 
-function animateCorner() {
-    const cornerElement = document.querySelector('.post-it__corner');
-    cornerElement.classList.toggle('--animatecorner');
-}
+// function animateCorner() {
+//     const cornerElement = document.querySelector('.post-it__corner');
+//     cornerElement.classList.toggle('--animatecorner');
+// }
 
-function toggleVisibility() {
-    const navElement = document.querySelector('.post-it__nav');
-    navElement.classList.toggle('--toggleVisibility');
-}
+// function toggleVisibility() {
+//     const navElement = document.querySelector('.post-it__nav');
+//     navElement.classList.toggle('--toggleVisibility');
+// }
 
-// Attach the onclick event to trigger the animation
-document.addEventListener('DOMContentLoaded', () => {
-    const cornerElement = document.querySelector('.post-it__corner');
-    cornerElement.addEventListener('click', animateCorner);
-    cornerElement.addEventListener('click', toggleVisibility);
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const cornerElement = document.querySelector('.post-it__corner');
+//     cornerElement.addEventListener('click', animateCorner);
+//     cornerElement.addEventListener('click', toggleVisibility);
+// });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const cornerElement = document.querySelector('.close');
-    cornerElement.addEventListener('click', animateCorner);
-    cornerElement.addEventListener('click', toggleVisibility);
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const cornerElement = document.querySelector('.close');
+//     cornerElement.addEventListener('click', animateCorner);
+//     cornerElement.addEventListener('click', toggleVisibility);
+// });
 
 // TODO intégrer slider
 document.addEventListener("DOMContentLoaded", function () {
     const pages = document.querySelectorAll(".post-it__page");
     let currentPageIndex = 0;
+
+    function setActivePage(index) {
+        switch (index) {
+            case 0:
+                idee.classList.add("--active");
+                dispositif.classList.remove("--active");
+                avantages.classList.remove("--active");
+                projets.classList.remove("--active");
+                produit.classList.remove("--active");
+                break;
+            case 1:
+                idee.classList.remove("--active");
+                dispositif.classList.add("--active");
+                avantages.classList.remove("--active");
+                projets.classList.remove("--active");
+                produit.classList.remove("--active");
+                break;
+            case 2:
+                idee.classList.remove("--active");
+                dispositif.classList.remove("--active");
+                avantages.classList.add("--active");
+                projets.classList.remove("--active");
+                produit.classList.remove("--active");
+                break;
+            case 3:
+                idee.classList.remove("--active");
+                dispositif.classList.remove("--active");
+                avantages.classList.remove("--active");
+                projets.classList.add("--active");
+                produit.classList.remove("--active");
+                break;
+            case 4:
+                idee.classList.remove("--active");
+                dispositif.classList.remove("--active");
+                avantages.classList.remove("--active");
+                projets.classList.remove("--active");
+                produit.classList.add("--active");
+                break;
+        }
+    }
 
     function showPage(pageIndex) {
         pages.forEach((page, index) => {
@@ -44,6 +83,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (currentPageIndex < pages.length - 1) {
             currentPageIndex++;
             showPage(currentPageIndex);
+            setActivePage(currentPageIndex);
         }
     }
 
@@ -51,12 +91,43 @@ document.addEventListener("DOMContentLoaded", function () {
         if (currentPageIndex > 0) {
             currentPageIndex--;
             showPage(currentPageIndex);
+            setActivePage(currentPageIndex);
         }
     }
 
     const nextBtn = document.querySelector(".footer__next-btn");
     const prevBtn = document.querySelector(".footer__prev-btn");
+    const idee = document.querySelector("#idee");
+    const dispositif = document.querySelector("#dispositif");
+    const avantages = document.querySelector("#avantages");
+    const projets = document.querySelector("#projets");
+    const produit = document.querySelector("#produit");
 
     nextBtn.addEventListener("click", goToNextPage);
     prevBtn.addEventListener("click", goToPreviousPage);
+    idee.addEventListener("click", function () {
+        currentPageIndex = 0;
+        showPage(currentPageIndex);
+        setActivePage(currentPageIndex);
+    });
+    dispositif.addEventListener("click", function () {
+        currentPageIndex = 1;
+        showPage(currentPageIndex);
+        setActivePage(currentPageIndex);
+    });
+    avantages.addEventListener("click", function () {
+        currentPageIndex = 2;
+        showPage(currentPageIndex);
+        setActivePage(currentPageIndex);
+    });
+    projets.addEventListener("click", function () {
+        currentPageIndex = 3;
+        showPage(currentPageIndex);
+        setActivePage(currentPageIndex);
+    });
+    produit.addEventListener("click", function () {
+        currentPageIndex = 4;
+        showPage(currentPageIndex);
+        setActivePage(currentPageIndex);
+    });
 });
