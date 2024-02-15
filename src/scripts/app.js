40eac8e399516ee8c134706364ff59cd3ad20034
GlupyLoop <46475581+GlupyLoop@users.noreply.github.com>
2024-02-15 22:09:17 +0100

    carrousel done

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -103,4 +103,60 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+//carousel
+
+function moveToSelected(element) {
+    var selected;
+    if (element === "next") {
+      selected = $(".selected").next();
+    } else if (element === "prev") {
+      selected = $(".selected").prev();
+    } else {
+      selected = element;
+    }
+  
+    var next = $(selected).next();
+    var prev = $(selected).prev();
+    var prevSecond = $(prev).prev();
+    var nextSecond = $(next).next();
+  
+    $(selected).removeClass().addClass("selected");
+  
+    $(prev).removeClass().addClass("prev");
+    $(next).removeClass().addClass("next");
+  
+    $(nextSecond).removeClass().addClass("nextRightSecond");
+    $(prevSecond).removeClass().addClass("prevLeftSecond");
+  
+    $(nextSecond).nextAll().removeClass().addClass('hideRight');
+    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+}
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+            moveToSelected('prev');
+            break;
+  
+        case 39: // right
+            moveToSelected('next');
+            break;
+  
+        default: return;
+    }
+    e.preventDefault();
+});
+  
+$('#carousel div').click(function() {
+    moveToSelected($(this));
+});
+  
+$('#prev').click(function() {
+    moveToSelected('prev');
+});
+  
+$('#next').click(function() {
+    moveToSelected('next');
+});
+
 
